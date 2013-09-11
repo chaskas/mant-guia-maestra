475f9066fdcbbcfc6e793d94899d3bfc11ba9b86
chaskas <rodrigo@webdevel.cl>
2013-09-11 13:58:17 -0300

    agregado twitter bootstrap, admin app y modulo home para pruebas

---
+++ apps/admin/modules/home/actions/actions.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * home actions.
+ *
+ * @package    mant-guia-maestra
+ * @subpackage home
+ * @author     Rodrigo Campos H. rodrigo <at> webdevel <dot> cl
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class homeActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+  }
+}
---
+++ apps/admin/templates/layout.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php include_title() ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include_stylesheets() ?>
+  </head>
+  <body>
+    <?php echo $sf_content ?>
+
+    <?php include_javascripts() ?>
+  </body>
+</html>
