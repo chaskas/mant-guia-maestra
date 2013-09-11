d3561f191e0265e4379a3613b62a7635fd6ab498
chaskas <rodrigo@webdevel.cl>
2013-09-11 12:12:53 -0300

    proyecto creado

---
+++ lib/form/BaseForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base project form.
+ * 
+ * @package    mant-guia-maestra
+ * @subpackage form
+ * @author     Rodrigo Campos H. rodrigo <at> webdevel <dot> cl 
+ * @version    SVN: $Id: BaseForm.class.php 20147 2009-07-13 11:46:57Z FabianLange $
+ */
+class BaseForm extends sfFormSymfony
+{
+}
